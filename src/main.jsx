9da532cbf2da3fff9e8c11da34be9ef9e123a7f2
farzanapomy <farzanapomy56@gmail.com>
2026-07-30 19:47:40 +0600

    add-json-data

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 // import App from './App.jsx'
 import { RouterProvider } from "react-router/dom";
 import { router } from './routes/router.jsx';
+import BuilderProvider from './context/BuilderContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BuilderProvider>
+      <RouterProvider router={router} />
+    </BuilderProvider>
   </StrictMode>,
 )
